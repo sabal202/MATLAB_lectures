5e0cad1cfe84569c8af292a75f52b011af3e6185
Sabalevskiy Sergey <sabal2000@mail.ru>
2019-03-13 09:40:15 +0300

    homework and docs

--- lecture3_funcs/homework.m
+++ lecture3_funcs/homework.m
@@ -1,9 +1,9 @@
-it = input('Iterations number: ');
 x0 = input('Enter x0: ');
+x0 = mod(x0, 2*pi);
 r = x0;
 res = x0;
-for i = 1:it
+for i = 1:10
     r = r*-1*x0*x0/(2*i*(2*i + 1));
     res = res + r;
 end
-fprintf('sin(x0) = %f\n', res);
+fprintf('sin(x0) = %.4f\n', res);
